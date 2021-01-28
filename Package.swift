c65cd5ed5f21efa35811f1a4b086149981e16a12
obuseme <obusek@gmail.com>
2021-01-28 08:57:30 -0500

    Update Package.swift for swift tools version 5.3

--- Package.swift
+++ Package.swift
@@ -1,14 +1,17 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
 	name: "FolioReaderKit",
 	dependencies: [
-		.Package(url: "https://github.com/ZipArchive/ZipArchive.git", majorVersion: 2, minor: 1),
-		.Package(url: "https://github.com/cxa/MenuItemKit.git", majorVersion: 3, minor: 0),
-		.Package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", majorVersion: 0, minor: 6),
-		.Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 2),
-		.Package(url: "https://github.com/ArtSabintsev/FontBlaster.git", majorVersion: 4, minor: 0),
-		.Package(url: "https://github.com/fantim/JSQWebViewController.git", majorVersion: 6, minor: 1),
-		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 1),
+		.package(url: "https://github.com/ZipArchive/ZipArchive.git", majorVersion: 2, minor: 1),
+		.package(url: "https://github.com/cxa/MenuItemKit.git", majorVersion: 3, minor: 0),
+		.package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", majorVersion: 0, minor: 6),
+		.package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 2),
+		.package(url: "https://github.com/ArtSabintsev/FontBlaster.git", majorVersion: 4, minor: 0),
+		.package(url: "https://github.com/fantim/JSQWebViewController.git", majorVersion: 6, minor: 1),
+		.package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 1),
 	]
 )

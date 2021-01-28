a42a57ad861cd4c267d69be4511dffb511c465f1
obuseme <obusek@gmail.com>
2021-01-28 11:55:34 -0500

    Add FontBlaster dependency

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         // "ZipArchive", "MenuItemKit", "ZFDragableModalTransition", AEXML", "FontBlaster", "JSQWebViewController", "Realm"
         .target(
             name: "FolioReaderKit",
-            dependencies: ["AEXML", "SWCompression"],
+            dependencies: ["AEXML", "SWCompression", "FontBlaster"],
             path: "Source"),
     ]
 )
